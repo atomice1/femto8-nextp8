0503db14912a2556dd15cc3b5b2d9189a9d68036
Chris January <chris@atomice.net>
2025-12-18 17:33:22 +0000

    Make quit work when Lua code is hung
    femto8 SDL and Lua code runs in the same thread. This means if the
    Lua code runs for a long time the SDL runtime does not pump events
    which leads to a 'hung' window.
    
    Add a Lua hook that runs every so many instructions (5,0000), pumps
    the event queue, and checks for the SDL_QUIT event.
    
    Remove getchar in lua_print_error as it stops the event loop from
    running.

--- src/p8_lua.c
+++ src/p8_lua.c
@@ -1684,6 +1684,13 @@ void lua_register_functions(lua_State *L)
     lua_pushnumber(L, z8::fix32::frombits(0x55558000)); lua_setglobal(L, "\x99");  // 153 ▥
 }
 
+static void lua_event_pump_hook(lua_State *L, lua_Debug *ar)
+{
+    (void)L;
+    (void)ar;
+    p8_pump_events();
+}
+
 void lua_load_api()
 {
     if (!L)
@@ -1695,10 +1702,16 @@ void lua_load_api()
 
     lua_register_functions(L);
 
+    // Set debug hook to pump events every ~3000 instructions
+    // At 30MHz 16-bit CPU interpreting Lua, this is roughly every 100ms
+    lua_sethook(L, lua_event_pump_hook, LUA_MASKCOUNT, 3000);
+
     if (luaL_dostring(L, lua_api_string))
         lua_print_error("Error loading extended PICO-8 Api");
 
     lua_setpico8memory(L, m_memory);
+
+    lua_sethook(L, lua_event_pump_hook, LUA_MASKCOUNT, 5000);
 }
 
 void lua_shutdown_api()
@@ -1724,7 +1737,6 @@ void lua_print_error(const char *where)
             printf("%s\r\n", message);
         }
     }
-    getchar();
 }
 
 void lua_init_script(const char *script)
